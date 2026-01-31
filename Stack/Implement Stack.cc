647a52c2b23f1fdd3ef72a8f207ec132ed421819
osama mohamoud <wwwhdosama@gmail.com>
2026-01-31 15:40:25 +0200

    Auto commit: 2026-01-31 15:40:25

---
+++ Stack/Implement Stack.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// implementing stack using vector (dynamic stack)
+// stack --> LIFO
+
+template <class T> // to use the class whit any data type
+class clsStack
+{
+private:
+    vector<T> VStack;
+
+public:
+    // push
+    void Push(T Element)
+    {
+        VStack.push_back(Element);
+    }
+
+    // pop
+    void Pop()
+    {
+        if (this->Empty())
+            throw std::invalid_argument("UnderFlow!\a");
+        VStack.pop_back();
+    }
+
+    // top , the first const to avoid editing on return value 
+    const T &Top() const //--> read-only function
+    {
+        if (this->Empty())
+            throw std::invalid_argument("Empty Stack!\a");
+        return VStack.back();
+    }
+
+    // empty
+    bool Empty() const //--> read-only function
+    {
+        return (VStack.empty());
+    }
+
+   
+};
+
+int main()
+{
+    clsStack<short> Stack1;
+    Stack1.Push(10);
+    Stack1.Push(20);
+    Stack1.Push(30);
+
+    while (!Stack1.Empty())
+    {
+
+        cout << Stack1.Top() << endl;
+        Stack1.Pop();
+    }
+    //  Stack1.Pop();
+ 
+}
