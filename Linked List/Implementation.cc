7f6b3d2b9436ac539c7e2231a631569d17d9a10d
osama mohamoud <wwwhdosama@gmail.com>
2026-02-02 22:14:28 +0200

    Auto commit: 2026-02-02 22:14:28

--- Linked List/Implementation.cc
+++ Linked List/Implementation.cc
@@ -20,6 +20,17 @@ public:
     {
         this->PointerToTheNextNode = TheAddressOfNextNode;
     }
+
+    // Get
+    T GetValue() const
+    {
+        return this->Value;
+    }
+
+    Node * AddressOfNextNode() const  
+    {
+        return PointerToTheNextNode;
+    }
 };
 
 int main()
@@ -30,5 +41,20 @@ int main()
     // you should make pointer to access the heap memory
     Node<int> *Node1 = new Node(10);
     Node<int> *Node2 = new Node(20);
-    
+    Node<int> *Node3 = new Node(30);
+
+    Head = Node1; // make the Head have the address of Node1
+
+    // Connecting Nodes With Each Other
+    Node1->SetAddressOfNextNode(Node2);
+    Node2->SetAddressOfNextNode(Node3);
+    Node3->SetAddressOfNextNode(nullptr);
+
+    Node<int> *Temp = Head; // Temp Pointer to have the value(the address of Node1) to avoid changing the Head value
+
+    while (Temp != nullptr) // nullptr --> means the last Node
+    {
+        cout<< Temp->GetValue() <<"\n";
+        Temp = Temp->AddressOfNextNode();
+    }
 }
