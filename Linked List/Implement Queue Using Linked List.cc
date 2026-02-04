9ecd1b3754181c846ee873dc3b6c81e9c6750d1e
osama mohamoud <wwwhdosama@gmail.com>
2026-02-04 18:00:35 +0200

    Auto commit: 2026-02-04 18:00:34

--- Linked List/Implement Queue Using Linked List.cc
+++ Linked List/Implement Queue Using Linked List.cc
@@ -8,6 +8,11 @@ private:
     int Value;
 
 public:
+    Node()
+    {
+        Ptr = nullptr;
+        Value = 0;
+    }
     void SetTheAddressForTheNextNode(Node *Ptr)
     {
         this->Ptr = Ptr;
@@ -23,8 +28,43 @@ public:
     Node *GetTheAddressOfNextNode() { return this->Ptr; }
 };
 
+
 template <class T>
-class QueueInterface // Abstract Class - Contract 
+class clsLinkedListUnderlying
+{
+
+public:
+    static void InsertElementToTheLastNode(Node *&Head, T value)
+    {
+
+        Node *NNode = new Node();
+        NNode->SetValue(value);
+
+        // it is the first element to push
+        if (Head == nullptr)
+        {
+            Head = NNode;
+            NNode->SetTheAddressForTheNextNode(nullptr);
+            return;
+        };
+
+        Node *Current = Head;
+        while (Current->GetTheAddressOfNextNode() != nullptr)
+        {
+            Current = Current->GetTheAddressOfNextNode(); // move the current ptr to another node
+        };
+
+        Current->SetTheAddressForTheNextNode(NNode); // set the address of next node to new node instead of null (linking the nodes together)
+
+        NNode->SetTheAddressForTheNextNode(nullptr);
+    };
+
+
+
+};
+
+template <class T>
+class QueueInterface // Abstract Class - Contract
 {
     virtual void push() = 0;
     virtual void pop() = 0;
@@ -39,12 +79,25 @@ class queue : private QueueInterface<T>
 {
 private:
     Node *Head;
+    int count;
 
 public:
     queue()
     {
         Head = nullptr;
+        count = 0;
     }
+
+    void push(T Element) override
+    {
+        clsLinkedListUnderlying<T>::InsertElementToTheLastNode(Head, Element);
+        count++;
+    };
 
 
 };
+
+int main()
+{
+
+}
