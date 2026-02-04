ca7140b8a163d322a2d389771d32fd07026bcfbd
osama mohamoud <wwwhdosama@gmail.com>
2026-02-04 16:53:19 +0200

    Auto commit: 2026-02-04 16:53:18

---
+++ Linked List/Delete The Last Node .cc
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+private:
+    Node *Ptr;
+    int Value;
+
+public:
+    Node()
+    {
+        Ptr = nullptr;
+        Value = 0;
+    }
+    void SetTheAddressForTheNextNode(Node *Ptr)
+    {
+        this->Ptr = Ptr;
+    }
+
+    void SetValue(int Value)
+    {
+        this->Value = Value;
+    }
+
+    // Get
+    int GetValue() { return Value; }
+    Node *GetTheAddressOfNextNode() { return this->Ptr; }
+};
+
+// insert at the begging
+void InsertAtTheBegging() {
+
+};
+
+void DeleteTheLastNode(Node *Head)
+{
+    if (Head == nullptr)
+        return; // the linked list is empty
+
+    if (Head->GetTheAddressOfNextNode() == nullptr) // there is one node only
+    {
+        delete Head;
+        return;
+    };
+    Node *Prev = Head;
+    Node *Current = Head;
+    while (Current->GetTheAddressOfNextNode() != nullptr) // traversing until the last node
+    {
+        Prev = Current;
+        Current = Current->GetTheAddressOfNextNode(); // move to the next node
+    };
+
+    Prev->SetTheAddressForTheNextNode(nullptr);
+    delete Current;
+};
+
+
+void PrintList(Node *Head)
+{
+    while (Head != nullptr)
+    {
+        cout << Head->GetValue() << endl;
+        Head = Head->GetTheAddressOfNextNode(); // move to the next address
+    };
+};
+
+int main()
+{
+        Node *Head = nullptr;
+    Node *Node1 = new Node();
+    Node *Node2 = new Node();
+    Node *Node3 = new Node();
+    Node *Node4 = new Node();
+
+    Node1->SetValue(10);
+    Node2->SetValue(20);
+    Node3->SetValue(30);
+    Node4->SetValue(40);
+
+    Node1->SetTheAddressForTheNextNode(Node2);
+    Node2->SetTheAddressForTheNextNode(Node3);
+    Node3->SetTheAddressForTheNextNode(Node4);
+    Node4->SetTheAddressForTheNextNode(nullptr);
+
+    Head = Node1;
+
+    cout << "\nBefore Deleting The Last Node\n";
+    PrintList(Head);
+
+    cout << "\nAfter Deleting The Last Node\n";
+    DeleteTheLastNode(Head);
+    PrintList(Head);
+}
